abd3fde665489e63dd8f5c014f86ac16d9d521f2
0Miquel <48658941+0Miquel@users.noreply.github.com>
2021-04-21 21:52:50 +0200

    Add files via upload

---
+++ coppelia_fun.py
@@ -0,0 +1,30 @@
+import sim          # librería para conectar con CoppeliaSim
+import sympy as sp  # librería para cálculo simbólico
+
+def connect(port):
+# Establece la conexión a VREP
+# port debe coincidir con el puerto de conexión en VREP
+# retorna el número de cliente o -1 si no puede establecer conexión
+    sim.simxFinish(-1) # just in case, close all opened connections
+    clientID=sim.simxStart('127.0.0.1',port,True,True,2000,5) # Conectarse
+    if clientID == 0: print("conectado a", port)
+    else: print("no se pudo conectar")
+    return clientID
+
+# Función para abrir o cerrar la pinza remotamente:
+def gripper(clientID, val):
+# función que acciona el efector final remotamente
+# val es Int con valor 0 ó 1 para desactivar o activar el actuador final. res,retInts,retFloats,retStrings,retBuffer=sim.simxCallScriptFunction(clientID,
+    res,retInts,retFloats,retStrings,retBuffer=sim.simxCallScriptFunction(clientID, "ROBOTIQ_85", sim.sim_scripttype_childscript,"gripper",[val],[],[],"", sim.simx_opmode_blocking)
+    return res
+
+def get_ids(clientID):
+    retCode, grip = sim.simxGetObjectHandle(clientID, 'ROBOTIQ_85', sim.simx_opmode_blocking)
+    retCode, joint1 = sim.simxGetObjectHandle(clientID, 'Joint1', sim.simx_opmode_blocking)
+    retCode, joint2 = sim.simxGetObjectHandle(clientID, 'Joint2', sim.simx_opmode_blocking)
+    retCode, joint3 = sim.simxGetObjectHandle(clientID, 'Joint3', sim.simx_opmode_blocking)
+    retCode, joint4 = sim.simxGetObjectHandle(clientID, 'Joint4', sim.simx_opmode_blocking)
+    retCode, joint5 = sim.simxGetObjectHandle(clientID, 'Joint5', sim.simx_opmode_blocking)
+    retCode, dummy = sim.simxGetObjectHandle(clientID, 'Dummy', sim.simx_opmode_blocking)
+
+    return grip, joint1, joint2, joint3, joint4, joint5, dummy
---
+++ movement_fun.py
@@ -0,0 +1,60 @@
+import math
+import numpy as np
+import sim          # librería para conectar con CoppeliaSim
+import sympy as sp  # librería para cálculo simbólico
+import time
+
+def inverse_kinematics(x,y,z):
+    cabGrados = 0
+    b = 0.2  # longitud de brazo mm
+    ab = 0.2  # longitud de antebrazo mm
+    m = 0.1 + 0.145  # longitud de muñequilla mm + pinza
+    H = 0.2  # altura de base mm
+
+    cabRAD = cabGrados * np.pi / 180
+    Axis1 = math.atan2(y, x)
+    M = math.sqrt(pow(x, 2) + pow(y, 2))
+    xprima = M
+    yprima = z
+    Afx = math.cos(cabRAD) * m
+
+    B = xprima - Afx
+    Afy = math.sin(cabRAD) * m
+    A = yprima + Afy - H
+    Hip = math.sqrt(pow(A, 2) + pow(B, 2))
+    alfa = math.atan2(A, B)
+    beta = math.acos((pow(b, 2) - pow(ab, 2) + pow(Hip, 2)) / (2 * b * Hip))
+    Axis2 = alfa + beta
+    gamma = math.acos((pow(b, 2) + pow(ab, 2) - pow(Hip, 2)) / (2 * b * ab))
+    Axis3 = gamma
+    Axis4 = 2 * np.pi - cabRAD - Axis2 - Axis3
+
+    Axis1Grados = (Axis1 * 180 / np.pi)  # Giro base en Grados
+    Axis2Grados = (90 - Axis2 * 180 / np.pi)  # Giro brazo en Grados
+    Axis3Grados = (180 - Axis3 * 180 / np.pi)  # Giro antebrazo grados
+    Axis4Grados = (180 - Axis4 * 180 / np.pi)  # Giro muñequilla grados
+
+    Axis2Grados = 90 + abs(90 - Axis2Grados)
+    Axis3Grados = -Axis3Grados
+    Axis4Grados = -Axis4Grados
+
+    return Axis1Grados, Axis2Grados, Axis3Grados, Axis4Grados
+
+def sort_degrees(Axis1Grados, Axis2Grados, Axis3Grados, Axis4Grados, joint1, joint2, joint3, joint4):
+    list_grados = [(Axis1Grados, joint1), (Axis2Grados, joint2), (Axis3Grados, joint3), (Axis4Grados, joint4)]
+    list_grados = sorted(list_grados, key=lambda x: x[0], reverse=True)
+    return list_grados
+
+def move_to(clientID, list_grados):
+    for grados, joint in list_grados:
+        retCode = sim.simxSetJointTargetPosition(clientID, joint, grados * np.pi / 180, sim.simx_opmode_oneshot)
+        time.sleep(1)
+
+def move_home(clientID, joint1, joint2, joint3, joint4):
+    retCode = sim.simxSetJointTargetPosition(clientID, joint3, 0 * np.pi / 180, sim.simx_opmode_oneshot)
+    time.sleep(0.5)
+    retCode = sim.simxSetJointTargetPosition(clientID, joint1, 0 * np.pi / 180, sim.simx_opmode_oneshot)
+    time.sleep(0.5)
+    retCode = sim.simxSetJointTargetPosition(clientID, joint2, 180 * np.pi / 180, sim.simx_opmode_oneshot)
+    time.sleep(0.5)
+    retCode = sim.simxSetJointTargetPosition(clientID, joint4, 0 * np.pi / 180, sim.simx_opmode_oneshot)
